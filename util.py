b921d978ada5abde7d40bb0af70f15cbb5a723a2
SeonghoBaek <seonghobaek@gmail.com>
2020-08-05 15:18:52 +0900

    Update util.py

--- util.py
+++ util.py
@@ -1,8 +1,19 @@
+# Adversarial Anomaly Detection
+#   - Utility functions
+#
+# Author: Seongho Baek
+# Contact: seongho.baek@sk.com
+#
+# For SKhynix only.
+# ==============================================================================
+
 import math
 import numpy as np
 import tensorflow as tf
 import errno
 import os
+import copy
+
 
 def get_batch(X, X_, size):
     # X, X_ must be nd-array
@@ -120,3 +131,28 @@ def mkdirP(path):
             pass
         else:
             raise
+
+
+class ImagePool(object):
+    def __init__(self, maxsize=50):
+        self.maxsize = maxsize
+        self.num_img = 0
+        self.images = []
+
+    def __call__(self, image):
+        if self.maxsize <= 0:
+            return image
+        if self.num_img < self.maxsize:
+            self.images.append(image)
+            self.num_img += 1
+            return image
+        if np.random.rand() > 0.5:
+            idx = int(np.random.rand()*self.maxsize)
+            tmp1 = copy.copy(self.images[idx])[0]
+            self.images[idx][0] = image[0]
+            idx = int(np.random.rand()*self.maxsize)
+            tmp2 = copy.copy(self.images[idx])[1]
+            self.images[idx][1] = image[1]
+            return [tmp1, tmp2]
+        else:
+            return image
